d3c061a87ae5ccbd05afa3613f65cc221b42d666
Evan Anger <devandanger@gmail.com>
2018-12-12 23:12:03 -0500

    Added window for these issues

--- public/electron.js
+++ public/electron.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const isDev = require('electron-is-dev');
+const { BrowserWindow, Menu } = require("electron");
 
 var menubar = require('menubar')
-var htmlToLoad = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+var htmlToLoad = isDev ? 'http://localhost:3000/index.html' : `file://${path.join(__dirname, '../build/index.html')}`
 
 
 var mb = menubar({ index: htmlToLoad })
@@ -12,6 +13,30 @@ mb.on('ready', function ready () {
   console.log('app is ready')
   // your app code here
 })
+
+//right click menu for Tray
+mb.on('after-create-window', function() {
+  const contextMenu = Menu.buildFromTemplate ([
+    {label: 'Settings', click: () => createSettingsWindow() },
+    {label: 'Restart', click: () => { mb.app.quit();mb.app.relaunch(); }},
+    {type: 'separator'},
+    {label: 'Quit', click: () => {mb.app.quit ();}}
+  ])
+  mb.tray.on ('right-click', () => {
+      mb.tray.popUpContextMenu (contextMenu);
+  })
+});
+
+const createSettingsWindow = () => {
+  console.log("Create settings")
+  let win = new BrowserWindow({ width: 200, height: 100 })
+  win.on('closed', () => {
+    win = null
+  })
+  var settingsHtmlToLoad = isDev ? 'http://localhost:3000/settingsWindow.html' : `file://${path.join(__dirname, '../build/settingsWindow.html')}`
+  win.loadURL(settingsHtmlToLoad)
+}
+
 
 // app.on('ready', () => {
 // 	createWindow();
